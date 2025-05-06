c823913a0836b5373e89d0f8c07002caf82cea45
Jaya <jayapavanip@gmail.com>
2025-05-06 13:47:47 -0400

    Adding initial configurations for frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">Welcome to MediQueue 👨‍⚕️</h1>
+    </div>
+  );
+}
+
+export default App
